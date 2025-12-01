457584e2d4b53865dfca776e520f6967cff26430
Ana Barea <ana_luiza_barea_melo@eaportal.or>
2025-12-01 17:08:33 -0300

    mudando o link

--- pages/cadastro.js
+++ pages/cadastro.js
@@ -16,7 +16,7 @@ async function sendUser(){
         password
     }
 
-    const response = await fetch("http://localhost:3333/cadastrar", {
+    const response = await fetch("https://pessoas-backend-2jcy.vercel.app/cadastrar", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
